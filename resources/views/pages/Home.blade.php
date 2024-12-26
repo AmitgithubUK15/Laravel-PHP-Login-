62961645873dc3c0557f3f0f4f2ce003cb268af6
Amit <Amitshah050112003@gmail.com>
2024-12-26 23:35:16 +0530

    setup tailwind css

--- resources/views/pages/Home.blade.php
+++ resources/views/pages/Home.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>Hii, I am client...</h1>
+    <h1 class="text-3xl text-red-500" >hii, i am from</h1>
 </body>
 </html>
